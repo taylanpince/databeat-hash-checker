b2f7399b449153205a3b8c89f7833bf998d8a900
Taylan Pince <taylanpince@gmail.com>
2024-12-04 13:39:02 +0100

    Simplify output for testing

--- src/index.ts
+++ src/index.ts
@@ -16,34 +16,21 @@ const generateSaltedHashedAddress = (hash: string) => {
 export const server = http.createServer((req, res) => {
     const universalAddress = "0xa2A7cD4302836767D194e2321E34B834494e0a28";
     const embeddedAddress = "0x0C65AdfF1e2dd19F14A06dc0a5Eb23bf34FD6938";
-    const universalHash = "0713aff2656cc6d0cfebfefd44b8546710c7be153a08e92474";
-    const embeddedHash = "12b71815ba9fa230ad8a16e91df10f7bf8545a9ceb9f93094e";
     const mehmetEmbeddedAddress = "0x5f21149eD1c4de522eB4414FC064f1DeF39f6c43";
-    const mehmetEmbeddedHash = "8219a557a4a22bf19a97e39e56e2a4a5f2128731a5472390a3";
 
     const hashedUniversalAddress = generateSaltedHashedAddress(generateHashedAddress(universalAddress));
     const hashedEmbeddedAddress = generateSaltedHashedAddress(generateHashedAddress(embeddedAddress));
     const hashedMehmetEmbeddedAddress = generateSaltedHashedAddress(generateHashedAddress(mehmetEmbeddedAddress));
 
-    const universalHashCorrect = hashedUniversalAddress === universalHash;
-    const embeddedHashCorrect = hashedEmbeddedAddress === embeddedHash;
-    const mehmetEmbeddedHashCorrect = hashedMehmetEmbeddedAddress === mehmetEmbeddedHash;
-
     res.writeHead(200, { "Content-Type": "application/json" });
     res.end(
         JSON.stringify({
             universalAddress,
-            embeddedAddress,
-            mehmetEmbeddedAddress,
-            universalHash,
-            embeddedHash,
-            mehmetEmbeddedHash,
             hashedUniversalAddress,
+            embeddedAddress,
             hashedEmbeddedAddress,
+            mehmetEmbeddedAddress,
             hashedMehmetEmbeddedAddress,
-            universalHashCorrect,
-            embeddedHashCorrect,
-            mehmetEmbeddedHashCorrect
         })
     );
 });
